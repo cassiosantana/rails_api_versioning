a0a10f3c7217b013487320e66c858bbfab8dd644
Cassio Santana <cassioo.roger@gmail.com>
2024-07-31 10:01:09 -0300

    feat: add ebook

---
+++ db/migrate/20240731125910_create_ebooks.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreateEbooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :ebooks do |t|
+      t.string :title
+      t.text :description
+      t.string :author
+      t.string :genre
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+end
