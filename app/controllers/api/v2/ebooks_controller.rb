6f08946ec43e274dc61618dc7c6f42cd24669669
Cassio Santana <cassioo.roger@gmail.com>
2024-08-01 19:42:13 -0300

    feat: permit new `publisher` attribute on v2
    Ref #4

--- app/controllers/api/v2/ebooks_controller.rb
+++ app/controllers/api/v2/ebooks_controller.rb
@@ -3,6 +3,11 @@
 module Api
   module V2
     class EbooksController < BaseEbooksController
+      private
+
+      def ebook_params
+        params.require(:ebook).permit(:title, :description, :author, :genre, :isbn, :publisher)
+      end
     end
   end
 end
