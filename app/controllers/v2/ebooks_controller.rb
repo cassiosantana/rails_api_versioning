73b1d110365f1bad18077c1e4011021233cf8ee8
Cassio Santana <cassioo.roger@gmail.com>
2024-08-12 00:39:38 -0300

    refactor: removes complexity from ebook serialization
    Ref #3 #18 #21

--- app/controllers/v2/ebooks_controller.rb
+++ app/controllers/v2/ebooks_controller.rb
@@ -1,11 +1,33 @@
 # frozen_string_literal: true
 
 module V2
-  class EbooksController < Base::EbooksController
+  class EbooksController < ApplicationController
+    def index
+      @ebooks = Ebook.all
+      render json: V2::EbooksSerializer.new(@ebooks).serializable_hash
+    end
+
+    def show
+      @ebook = Ebook.find(params[:id])
+      render json: V2::EbooksSerializer.new(@ebook).serializable_hash
+    rescue ActiveRecord::RecordNotFound
+      render json: JsonResponses::Errors::NotFound.call(Ebook.name), status: :not_found
+    end
+
+    def create
+      @ebook = Ebook.new(ebook_params)
+
+      if @ebook.save
+        render json: V2::EbooksSerializer.new(@ebook).serializable_hash, status: :created
+      else
+        render json: JsonResponses::Errors::Validation.call(@ebook.errors), status: :unprocessable_entity
+      end
+    end
+
     private
 
     def ebook_params
-      super.merge(params.require(:data).require(:attributes).permit(:publisher))
+      params.require(:data).require(:attributes).permit(:title, :description, :author, :genre, :isbn, :publisher)
     end
   end
 end
--- app/controllers/v3/ebooks_controller.rb
+++ app/controllers/v3/ebooks_controller.rb
@@ -1,25 +1,39 @@
 # frozen_string_literal: true
 
 module V3
-  class EbooksController < Base::EbooksController
+  class EbooksController < ApplicationController
     def index
       @ebooks = Ebook.page(params[:page]).per(5)
 
-      ebooks = JsonResponses::Ebooks::Response.call(self.class.name, @ebooks)
-      pagination = JsonResponses::Pagination.call(@ebooks)
-      links = JsonResponses::Links.call(@ebooks, request)
-
       render json: {
-        data: ebooks[:data],
-        meta: pagination,
-        links:
+        data: V3::EbooksSerializer.new(@ebooks).serializable_hash[:data],
+        meta: JsonResponses::Pagination.call(@ebooks),
+        links: JsonResponses::Links.call(@ebooks, request)
       }
+    end
+
+    def show
+      @ebook = Ebook.find(params[:id])
+      render json: V3::EbooksSerializer.new(@ebook).serializable_hash
+    rescue ActiveRecord::RecordNotFound
+      render json: JsonResponses::Errors::NotFound.call(Ebook.name), status: :not_found
+    end
+
+    def create
+      @ebook = Ebook.new(ebook_params)
+
+      if @ebook.save
+        render json: V3::EbooksSerializer.new(@ebook).serializable_hash,
+               status: :created
+      else
+        render json: JsonResponses::Errors::Validation.call(@ebook.errors), status: :unprocessable_entity
+      end
     end
 
     private
 
     def ebook_params
-      super.merge(params.require(:data).require(:attributes).permit(:publisher))
+      params.require(:data).require(:attributes).permit(:title, :description, :author, :genre, :isbn, :publisher)
     end
   end
 end
