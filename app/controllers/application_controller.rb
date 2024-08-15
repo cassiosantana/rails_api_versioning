018112cd0072dff2222b0e3796ab4bb8ceb675de
Cassio Santana <cassioo.roger@gmail.com>
2024-08-15 13:59:33 -0300

    feat(ebook): permit isbn13 in api version 1 and 2
    Ref #3 #19 #28

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :map_isbn_to_isbn13, only: :create
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
+
+  def map_isbn_to_isbn13
+    return unless params[:data][:attributes][:isbn].present?
+
+    params[:data][:attributes][:isbn13] = params[:data][:attributes].delete(:isbn)
+  end
 
   def not_found(exception)
     render json: JsonResponses::Errors::NotFound.call(exception), status: :not_found
